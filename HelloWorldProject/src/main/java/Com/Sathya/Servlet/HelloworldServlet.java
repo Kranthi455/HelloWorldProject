8f3a29b37d0e085674ca14955837b0dbe4501f51
kotakranthikumar <kotakranthikumar455@gmail.com>
2025-03-24 12:59:46 +0530

    first change

--- HelloWorldProject/src/main/java/Com/Sathya/Servlet/HelloworldServlet.java
+++ HelloWorldProject/src/main/java/Com/Sathya/Servlet/HelloworldServlet.java
@@ -20,7 +20,7 @@ public class HelloworldServlet extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		writer.println("<html>");
 //		writer.println("<Body bgcolor="+"aqua>");
-		writer.println("<h1><BACK COLOR=" + "RED>" + "I Am KranthiKumar</h1>");
+		writer.println("<h1><BACK COLOR=" + "RED>" + "I Am KranthiKumarkota</h1>");
 		writer.println("<h2><FONT COLOR=" + "BLUE>" + "Sathya Technology</h2>");
 		writer.println("<h3><FONT COLOR=" + "GREEN" + "This is Advanc java Class<h3> ");
 //		writer.println("</Body>");
